2f12282d012e062fbfd4a16a6af0333f01fadcaf
ArobyR <ar.rosario001@gmail.com>
2021-04-13 14:59:13 -0400

    add title and change style

--- resources/js/components/CardContainer/CardContainer.js
+++ resources/js/components/CardContainer/CardContainer.js
@@ -5,11 +5,13 @@ import logo_corriente from '../../../images/logo_corriente.svg';
 import logo_discapacitado from '../../../images/logo_discapacitado.svg';
 import Card from '../Card/Card';
 import ButtonClick from '../ButtonClick/ButtonClick';
+import '../../../css/global.css';
 
 const CardContainer = () => {
     return (
-        <div className='container'>
-            <div className='d-flex flex-wrap card-container'>
+        <div className='vh-100 container'>
+            <h2 className='title text-center'>Seleccione su estado:</h2>      
+            <div className='d-flex flex-wrap'>
                 <div className='d-flex mx-auto'>
                     <Card logo={logo_embarazada} textSide="embarazada" />
                 </div>
@@ -20,7 +22,7 @@ const CardContainer = () => {
                     <Card logo={logo_discapacitado} textSide="discapacitado" />
                 </div>
             </div>
-            <div className='row'>
+            <div className='row mt-5'>
                 <div className='mx-auto'>
                     <ButtonClick name="Generar codigo QR" />
                 </div>
