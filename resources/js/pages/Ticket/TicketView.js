77d3d728c0f44704c7385cc2fcee3ca60134e7f2
ArobyR <ar.rosario001@gmail.com>
2021-04-13 14:56:31 -0400

    add ticket page
    add TicketView, it has into a CardContainer

---
+++ resources/js/pages/Ticket/TicketView.js
@@ -0,0 +1,12 @@
+import CardContainer from "../../components/CardContainer/CardContainer"
+import '../../../css/global.css';
+
+const TicketView = () => {
+    return (
+        <div className='bg-color'>
+            <CardContainer />
+        </div>
+    );
+}
+ 
+export default TicketView;
