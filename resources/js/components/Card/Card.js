7b1ea282e8e4f8a5c758dc1b233bca0b87d77e36
ArobyR <ar.rosario001@gmail.com>
2021-04-15 08:49:29 -0400

    change style

--- resources/js/components/Card/Card.js
+++ resources/js/components/Card/Card.js
@@ -2,9 +2,11 @@ import './card.css'
 
 const Card = (props) => {
     return (
-        <div onClick={props.handleOnClick} className='card-style'> 
-            <img className='card-images mx-auto' src={props.logo}/>
-            <div className='card-text'>{props.textSide}</div>
+        <div className='d-flex mx-auto'>
+            <div onClick={props.handleOnClick} className='card-style'> 
+                <img className='card-images mx-auto' src={props.logo}/>
+                <div className='card-text'>{props.textSide}</div>
+            </div>
         </div>
     );
 }
