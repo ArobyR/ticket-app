b86a6d94565b7dc9f47d63004276ac19073be7a3
jamekock <eliasplaying@gmail.com>
2021-04-16 19:02:05 -0400

    Add migrations

---
+++ database/migrations/2021_04_15_140351_create_ticket_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket', function (Blueprint $table) {
+            $table->bigIncrements('id_ticket')->unsigned();
+		    $table->bigInteger('id_categoria_fk')->unsigned();
+            $table->string('prioridad');
+		    $table->string('codigo_ticket',22);
+		    $table->foreign('id_categoria_fk')->references('id_categoria')->on('categoria_ticket')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ticket');
+    }
+}
