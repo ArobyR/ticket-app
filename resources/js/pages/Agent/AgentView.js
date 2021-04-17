95bd9640810edfc383c97d098bde15e5df7bf2b3
ArobyR <ar.rosario001@gmail.com>
2021-04-17 10:22:12 -0400

    add agent page

---
+++ resources/js/pages/Agent/AgentView.js
@@ -0,0 +1,11 @@
+import AgentContainer from "../../components/AgentContainer/AgentContainer"
+
+const AgentView = () => {
+    return (
+        <>
+            <AgentContainer />
+        </>
+    );
+}
+ 
+export default AgentView;
