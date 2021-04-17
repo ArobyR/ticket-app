dd865fd7f20012dc3416e29ed504200e5effdf10
ArobyR <ar.rosario001@gmail.com>
2021-04-17 10:18:02 -0400

    add login component
    add login with popup component

---
+++ resources/js/components/Login/Login.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import {Formik, useFormik} from 'formik';
+import './login-form.css';
+import { Form, Button } from 'react-bootstrap';
+
+const Login = () => {
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (evt) => {
+        const form = evt.currentTarget;
+        if(form.checkValidity() === false) {
+            evt.preventDefault(); 
+            evt.stopPropagation();
+        }
+        console.log('logeado...')
+        setValidated(true)
+    }
+
+    return (
+        <div className="vh-100 container">
+            <div className="form">
+                <h3 className="text-center mb-4 mt-4">Ingrese su cuenta</h3>
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                    <Form.Group controlId="validationFormik01">
+                        <Form.Label>Email:</Form.Label>
+                        <Form.Control required type="email" placeholder="Introduce tu email"/>
+                    </Form.Group>
+                    <Form.Group controlId="validationFormik02">
+                        <Form.Label style={{marginTop: '10px'}}>Contrasenia:</Form.Label>
+                        <Form.Control required type="password" placeholder="Introduce tu contrasenia"/>
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a valid pass.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                    <Button style={{marginTop: '30px'}}
+                        className='btn-style' 
+                        type='submit' 
+                        >
+                            Enviar
+                    </Button>
+                </Form>
+            </div>
+        </div>
+    );
+}
+ 
+export default Login;
---
+++ resources/js/pages/Login/LoginView.js
@@ -0,0 +1,11 @@
+import Login from "../../components/Login/Login"
+
+const LoginView = () => {
+    return (
+        <div className='bg-color'>
+            <Login />
+        </div>
+    );
+}
+ 
+export default LoginView;
