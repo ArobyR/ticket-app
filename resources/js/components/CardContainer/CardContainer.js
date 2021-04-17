0e9a216e5299e02e5b56a0bacd08d90d79380d3a
ArobyR <ar.rosario001@gmail.com>
2021-04-17 10:23:38 -0400

    add n changes

--- resources/js/components/CardContainer/CardContainer.js
+++ resources/js/components/CardContainer/CardContainer.js
@@ -6,9 +6,18 @@ import logo_discapacitado from '../../../images/logo_discapacitado.svg';
 import Card from '../Card/Card';
 import ButtonClick from '../ButtonClick/ButtonClick';
 import '../../../css/global.css';
+import Popup from '../Popup/Popup';
 
 const CardContainer = () => {
+    const modalInitialValues = {
+        text: 'Ticket impreso!!', 
+        url: null
+    }
+
     const [cuality, setCuality] = useState({category: 'corriente'})
+    const [modalShow, setModalShow] = useState(false)
+    const [modalInfo, setmodalInfo] = useState(modalInitialValues)
+
     const handleEmbarazada = () => {
         setCuality({...cuality, category: 'embarazada'})
     }
@@ -22,11 +31,25 @@ const CardContainer = () => {
     }
 
     const printJson = () => {
+        setmodalInfo({...modalInfo, modalInitialValues})
+        setModalShow(true)
         console.log(cuality)
+    }
+
+    const test = () => {
+        setmodalInfo({...modalInfo, text: 'JO JO JO', url: logo_corriente})
+        setModalShow(true)
+
     }
 
     return (
         <div className='vh-100 container'>
+            <Popup 
+                text={modalInfo.text}
+                image={modalInfo.url}
+                show={modalShow}
+                onHide={() =>setModalShow(false)}
+            />
             <h2 className='title text-center'>Seleccione su estado:</h2>      
             <div className='d-flex flex-wrap'>
                 <Card logo={logo_embarazada} 
@@ -44,7 +67,7 @@ const CardContainer = () => {
             </div>
             <div className='row mt-5'>
                 <div className='mx-auto'>
-                    <ButtonClick name="Generar codigo QR" />
+                    <ButtonClick name="Generar codigo QR" handleOnClick={test} />
                 </div>
                 <div className='mx-auto'>
                     <ButtonClick handleOnClick={printJson} name="Imprimir ticket" />
