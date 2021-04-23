bc0ccd17f8c0b5be6f32e642ee745abd3d76cc6e
ArobyR <ar.rosario001@gmail.com>
2021-04-23 09:08:29 -0400

    add shift components

---
+++ resources/js/components/ShiftItemList/ShiftItemList.js
@@ -0,0 +1,11 @@
+import './shift-item.css'
+
+const ShiftItemList = ({ data }) => {
+    return (
+        <div className='item-container'>
+            <p className='item-title'>Turno x</p>
+        </div>
+    );
+}
+ 
+export default ShiftItemList;
---
+++ resources/js/components/ShiftListContainer/ShiftListContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import CarouselContainer from "../CarouselContainer/CarouselContainer"
+import ShiftList from "../ShiftList/ShiftList";
+
+const ShiftListContainer = () => {
+    return (
+        <React.Fragment>
+            <CarouselContainer />
+            <ShiftList />
+        </React.Fragment>
+    );
+}
+ 
+export default ShiftListContainer;
